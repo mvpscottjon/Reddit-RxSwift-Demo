d4dde1214fbd4dcd21a666c2ac48a36d2a2c5e0f
Seven <mvpscottjon@gmail.com>
2021-10-07 23:00:09 +0800

    init files

---
+++ Reddit-RxSwiftDemo/Reddit-RxSwiftDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reddit-RxSwiftDemo
+//
+//  Created by Seven on 2021/10/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Reddit-RxSwiftDemo/Reddit-RxSwiftDemoTests/Reddit_RxSwiftDemoTests.swift
@@ -0,0 +1,33 @@
+//
+//  Reddit_RxSwiftDemoTests.swift
+//  Reddit-RxSwiftDemoTests
+//
+//  Created by Seven on 2021/10/7.
+//
+
+import XCTest
+@testable import Reddit_RxSwiftDemo
+
+class Reddit_RxSwiftDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
